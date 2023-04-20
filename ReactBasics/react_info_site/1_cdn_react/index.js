81c47800e82e9d7fde1ccd0beac2d89732f42eed
Gaurab Lenovo <gaurab2193@gmail.com>
2023-04-20 13:37:03 +0530

    react

---
+++ ReactBasics/react_info_site/1_cdn_react/index.js
@@ -0,0 +1,10 @@
+ReactDOM.render(
+  <div>
+    <h1>Starting React</h1>
+    <ul>
+      <li>Faith on Shivbaba</li>
+      <li>Success</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
